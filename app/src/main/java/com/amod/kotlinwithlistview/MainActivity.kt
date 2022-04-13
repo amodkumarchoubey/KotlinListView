6569b84d248e2330e123d9d6396bf30b8093318b
amodkumarchoubey <amodchoubey@gmail.com>
2022-04-13 17:30:53 +0530

    Initial commit

---
+++ app/src/main/java/com/amod/kotlinwithlistview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.amod.kotlinwithlistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // use arrayadapter and define an array
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+        // access the listView from xml file
+        var mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+    }
+
+}
